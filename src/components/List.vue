9bfa478535434b437030c211971d0f687a3d8644
Peter <peter@peter-debian.local>
2018-12-26 16:13:48 +0200

    add template syntax

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,8 +2,15 @@
   <div class="list">
     <br>
     <h3>List of objectives: {{ val }}</h3>
+    <span v-if="seen">just some objectives to help the learning process</span>
+    <br>
     <p>create components</p>
     <p>learn to use components</p>
+    <p>style guide</p>
+    <p>interpolations: mustache braces | v-html | v-bind:id="dynamicId"</p>
+    <p>directives: v-if="seen"</p>
+    <p>arguments: v-on:click="doSomething"</p>
+    <p>shorthand: @click="doSomething"</p>
     <p>learn from youtube videos</p>
     <p>create a bearable website with vue</p>
     <p>learn aws</p>
@@ -15,14 +22,21 @@ export default {
   name: "List",
   props: {
     val: Number
+  },
+  data() {
+    return {
+      seen: true
+    };
   }
 };
 </script>
 
 <style scoped>
 p {
-  color: #42b983;
-  display: inline-block;
-  padding: 10px;
+  color: cornflowerblue;
+}
+
+.list {
+  background: lightgray;
 }
 </style>
