e9aa91144ba171f23874d3c91d8dfd537ba3fd32
Peter <peter@peter-debian.local>
2018-12-26 11:47:25 +0200

    add views

---
+++ src/views/Peter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="peter">
+    <h1>Peter's home page</h1>
+    <List val="5"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import List from "@/components/List.vue";
+
+export default {
+  name: "peter",
+  components: {
+    List
+  }
+};
+</script>
