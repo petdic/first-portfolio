1843601316b50be7dfec5a704926f851d36d7c2a
Peter <peter@peter-debian.local>
2018-12-26 11:50:56 +0200

    edit view to include component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <List val="5"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import List from "@/components/List.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    HelloWorld,
+    List
   }
 };
 </script>
