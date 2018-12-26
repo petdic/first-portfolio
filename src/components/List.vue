683b61b9852550a4d75e3b2691f77f1d6b959e23
Peter <peter@peter-debian.local>
2018-12-26 11:47:59 +0200

    add List component

---
+++ src/components/List.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="list">
+    <br>
+    <h3>List of objectives: {{ val }}</h3>
+    <p>create components</p>
+    <p>learn to use components</p>
+    <p>learn from youtube videos</p>
+    <p>create a bearable website with vue</p>
+    <p>learn aws</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+  props: {
+    val: Number
+  }
+};
+</script>
+
+<style scoped>
+p {
+  color: #42b983;
+  display: inline-block;
+  padding: 10px;
+}
+</style>
